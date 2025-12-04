de41ca5ddd991776edee46879c8ccecfcaaa95d2
Craig Vandervelden <craigvandervelden@gmail.com>
2025-12-03 21:05:09 -0600

    terminal theme by hostname

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,8 +1,11 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- Load the wez-tmux plugin for tmux-like keybindings
-local wez_tmux = wezterm.plugin.require 'https://github.com/sei40kr/wez-tmux.git'
+local wez_tmux = wezterm.plugin.require("https://github.com/sei40kr/wez-tmux.git")
+
+-- bar
+local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
 
 -- This will hold our configuration
 local config = {}
@@ -10,13 +13,11 @@ local config = {}
 -- In newer versions of WezTerm, we need to check for and use config.color_schemes
 -- This is the recommended way to add custom themes
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- Configure your leader key (recommended to avoid conflicts)
-config.leader = { key = "a", mods = "CTRL" }  -- Use Ctrl+a instead of default Ctrl+b
-
-
+config.leader = { key = "a", mods = "CTRL" } -- Use Ctrl+a instead of default Ctrl+b
 
 -- -----------------------------------------------------------------------------
 -- User Parameters:
@@ -24,52 +25,44 @@ config.leader = { key = "a", mods = "CTRL" }  -- Use Ctrl+a instead of default C
 -- -----------------------------------------------------------------------------
 
 -- Settings for local (non-SSH) sessions
-local local_params = {
-  color_scheme = 'kanagawabones',
-  background_image_path = '/Dropbox/Pictures/random-backgrounds/wallhaven-we2l2p.png',
-  font = 'FiraCode Nerd Font',
-  font_size = 12.0,
-  background_tint_opacity = 0.85,
+local hostname_params = {
+	["lupa"] = {
+		color_scheme = "kurokula",
+		background_image_path = "/Dropbox/Pictures/random-backgrounds/lupa/lupa2.jpg",
+		initial_background_image = "/Dropbox/Pictures/random-backgrounds/lupa/lupa2.png",
+		font = "FiraCode Nerd Font",
+		font_size = 12.0,
+		background_tint_opacity = 0.83,
+	},
+	["iuno"] = {
+		color_scheme = "tokyonight",
+		background_image_path = "/Dropbox/Pictures/random-backgrounds/iuno/iuno5.jpg",
+		initial_background_image = "/Dropbox/Pictures/random-backgrounds/iuno/iuno3.jpg",
+		font = "FiraCode Nerd Font",
+		font_size = 12.0,
+		background_tint_opacity = 0.85,
+	},
+  ["cartethyia"] = {
+
+  }
 }
 
 -- Settings for SSH sessions
 local ssh_params = {
-  color_scheme = 'tokyonight',  -- Can be different from local
-  background_image_path = '/Dropbox/Pictures/random-backgrounds/lupa.png',
-  font = 'FiraCode Nerd Font',  -- Can be different from local
-  font_size = 12.0,  -- Can be different from local
-  background_tint_opacity = 0.85,  -- Can be different from local
+  color_scheme = hostname_params["lupa"].color_scheme,
+  background_image_path = hostname_params["lupa"].background_image_path,
+  font = hostname_params["lupa"].font,
+  font_size = hostname_params["lupa"].font_size,
+  background_tint_opacity = 0.85, -- Override this one
 }
 
--- -----------------------------------------------------------------------------
--- Theme Definitions:
--- Add or modify color schemes here.
--- -----------------------------------------------------------------------------
-config.color_schemes = {
-  tokyonight = {
-    foreground         = '#D5DAF3',
-    background         = '#0F1016',
-    cursor_bg          = '#C0CAF5',
-    cursor_border      = '#C0CAF5',
-    cursor_fg          = '#0F1016',
-    selection_bg       = '#33467C',
-    selection_fg       = '#D5DAF3',
-    ansi               = { '#15161E', '#F7768E', '#9ECE6A', '#E0AF68', '#7AA2F7', '#BB9AF7', '#7DCFFF', '#C0C4D6' },
-    brights            = { '#B9BFD9', '#F7768E', '#9ECE6A', '#E0AF68', '#7AA2F7', '#BB9AF7', '#7DCFFF', '#D2D8F2' },
-  },
+-- Get the current hostname to select appropriate parameters
+local hostname = wezterm.hostname()
+if not hostname_params[hostname] then
+    hostname = "lupa"
+end
 
-  kanagawabones = {
-    foreground         = '#ddd8bb',
-    background         = '#1f1f28',
-    cursor_bg          = '#e6e0c2',
-    cursor_border      = '#e6e0c2',
-    cursor_fg          = '#1f1f28',
-    selection_bg       = '#49473e',
-    selection_fg       = '#ddd8bb',
-    ansi               = { '#1f1f28', '#e46a78', '#98bc6d', '#e5c283', '#7eb3c9', '#957fb8', '#7eb3c9', '#ddd8bb' },
-    brights            = { '#3c3c51', '#ec818c', '#9ec967', '#f1c982', '#7bc2df', '#a98fd2', '#7bc2df', '#a8a48d' },
-  },
-}
+local local_params = hostname_params[hostname]
 
 -- -----------------------------------------------------------------------------
 -- Apply User Parameters to Configuration
@@ -85,53 +78,69 @@ config.font_size = local_params.font_size
 -- -----------------------------------------------------------------------------
 -- This function detects if we're in an SSH session
 local function is_ssh_session(pane)
-  if not pane then
-    return false
-  end
+	if not pane then
+		return false
+	end
 
-  -- Check if we're in an SSH session by looking at the pane's user variables
-  local user_vars = pane:get_user_vars()
-  if user_vars.SSH_CONNECTION or user_vars.SSH_CLIENT or user_vars.SSH_TTY then
-    return true
-  end
+	-- Check if we're in an SSH session by looking at the pane's user variables
+	local user_vars = pane:get_user_vars()
+	if user_vars.SSH_CONNECTION or user_vars.SSH_CLIENT or user_vars.SSH_TTY then
+		return true
+	end
 
-  -- Also check the foreground process name
-  local foreground_process = pane:get_foreground_process_name()
-  if foreground_process and foreground_process:find('ssh') then
-    return true
-  end
+	-- Also check the foreground process name
+	local foreground_process = pane:get_foreground_process_name()
+	if foreground_process and foreground_process:find("ssh") then
+		return true
+	end
 
-  return false
+	return false
 end
 
 -- This function returns the appropriate background configuration
 local function get_background_for_params(params)
-  return {
-    -- First layer: the image (drawn first)
-    {
-      source = {
-        File = wezterm.home_dir .. params.background_image_path,
-      },
-      width = "Cover",
-      height = "Cover",
-    },
-    -- Second layer: a color tint (drawn on top of the image)
-    {
-      source = {
-        -- Dynamically get the background color from the active theme
-        Color = config.color_schemes[params.color_scheme].background,
-      },
-      -- Apply the desired opacity to this color layer
-      opacity = params.background_tint_opacity,
-      height = "100%",
-      width = "100%",
-    },
-  }
+	return {
+		-- First layer: the image (drawn first)
+		{
+			source = {
+				File = wezterm.home_dir .. params.background_image_path,
+			},
+			width = "Cover",
+			height = "Cover",
+		},
+		-- Second layer: a color tint (drawn on top of the image)
+		{
+			source = {
+				-- Dynamically get the background color from the active theme
+				Color = wezterm.color.get_builtin_schemes()[params.color_scheme].background,
+			},
+			-- Apply the desired opacity to this color layer
+			opacity = params.background_tint_opacity,
+			height = "100%",
+			width = "100%",
+		},
+	}
+end
+
+local function extend_color_scheme(hostname, config)
+  local tab_bar = config.colors.tab_bar or {}
+  -- Customize tab bar colors based on hostname
+  if hostname == "lupa" then
+    tab_bar.background = "#141515"
+    tab_bar.active_tab.bg_color = "#b66056"
+    tab_bar.active_tab.fg_color = "#333333"
+    tab_bar.inactive_tab.bg_color = "#333333"
+    tab_bar.inactive_tab.fg_color = "#b66056"
+    tab_bar.new_tab.bg_color = "#141515"
+    tab_bar.new_tab.fg_color = "#dbbb43"
+
+  end
+
+  return tab_bar
 end
 
 -- Set initial background (will be updated dynamically)
 config.background = get_background_for_params(local_params)
-
 -- -----------------------------------------------------------------------------
 -- Event Handler: Update Configuration on Window Focus
 -- -----------------------------------------------------------------------------
@@ -154,6 +163,20 @@ end)
 
 -- Apply wez-tmux plugin keybindings
 wez_tmux.apply_to_config(config)
+
+-- Apply bar plugin configuration
+
+bar.apply_to_config(config, {
+	position = "bottom",
+	modules = {
+		workspace = { enabled = false },
+		clock = { format = "%I:%M %p" },
+	},
+})
+
+-- Some color schemes don't have a fully fleshed out scheme,
+-- so they need to be manually extended.
+config.colors.tab_bar = extend_color_scheme(hostname, config)
 
 -- Finally, return the configuration
 return config
